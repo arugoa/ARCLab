aa161a877eb854f072eb3acf9b97222bed22e319
arugoa <arihantjaingoa@gmail.com>
2025-11-17 22:58:28 -0800

    working code!

--- ARCLab_laproscopy/ARCLab_laproscopy.ino
+++ ARCLab_laproscopy/ARCLab_laproscopy.ino
@@ -1,47 +1,170 @@
 #include "main.h"
 
 void setup() {
-  // Begin Serial output
   Serial.begin(115200);
-  Serial.println("1 to switch servos, 2 to switch direction of screw, 3 to turn off all motors");
-  
-  // Initializing LEDs
   pinMode(LED, OUTPUT);
+  digitalWrite(LED, LOW);
 
-  // Clamp Screw motor
-  pinMode(clampCW, OUTPUT);
-  pinMode(clampCCW, OUTPUT);
+  FastLED.addLeds<NEOPIXEL, leftLED>(leftLeds, NUMLED);
+  FastLED.addLeds<NEOPIXEL, rightLED>(rightLeds, NUMLED);
+  FastLED.setBrightness(128);
+  fill_solid(leftLeds, NUMLED, CRGB::Black);
+  fill_solid(rightLeds, NUMLED, CRGB::Black);
+  FastLED.show();
 
-  // Grip servo + init
-  gripServo.attach(gripPWM);
-  gripServo.write(init_grip);
+  // Left hand
+  pinMode(laserL, OUTPUT);
+  digitalWrite(laserL, LOW);
+  gripServoL.attach(gripPWML);
+  gripServoL.write(curr_grip_left);
+  lockServoL.attach(lockPWML);
+  lockServoL.write(curr_lock_left);
 
-  // Lock servo + init
-  lockServo.attach(lockPWM);
-  lockServo.write(init_lock);
+  // Right hand
+  pinMode(laserR, OUTPUT);
+  digitalWrite(laserR, LOW);
+  gripServoR.attach(gripPWMR);
+  gripServoR.write(curr_grip_right);
+  lockServoR.attach(lockPWMR);
+  lockServoR.write(curr_lock_right);
 
-  // Set up buttons
-  pinMode(button1, INPUT_PULLUP);
-  delayMicroseconds(10);
-  pinMode(button2, INPUT_PULLUP);
-  delayMicroseconds(10);
+  // Buttons
+  pinMode(laserButR, INPUT_PULLUP);
+  pinMode(gripButR, INPUT_PULLUP);
+  pinMode(lockButR, INPUT_PULLUP);
 
-  // Init motors to LOW
-  digitalWrite(clampCW, LOW);
-  digitalWrite(clampCCW, LOW);
+  pinMode(laserButL, INPUT_PULLUP);
+  pinMode(gripButL, INPUT_PULLUP);
+  pinMode(lockButL, INPUT_PULLUP);
+
+  Serial.println("Ready for input");
+}
+
+void updateLEDs() {
+  // -------- LEFT SIDE --------
+  fill_solid(leftLeds, NUMLED, CRGB::Black);
+
+  bool gripL = (curr_grip_left != 0);
+  bool lockL = (curr_lock_left != 0);
+
+  if (gripL) rightLeds[2] = CRGB::Blue;
+  if (lockL) rightLeds[3] = CRGB::Green;
+
+  // -------- RIGHT SIDE --------
+  fill_solid(rightLeds, NUMLED, CRGB::Black);
+
+  bool gripR = (curr_grip_right != 0);
+  bool lockR = (curr_lock_right != 0);
+
+  if (gripR) rightLeds[0] = CRGB::Blue;
+  if (lockR) rightLeds[1] = CRGB::Green;
+
+
+  FastLED.show();
+}
+
+void handleButtons() {
+  unsigned long now = millis();
+  if (now - lastButtonTime < debounceDelay) return;
+
+  // ----- RIGHT HAND -----
+  if (digitalRead(gripButR) == LOW) {       // Grip toggle
+    des_grip_right = (des_grip_right == 0) ? grip_limit : 0;
+    lastButtonTime = now;
+  }
+
+  if (digitalRead(lockButR) == LOW) {       // Lock toggle
+    des_lock_right = (des_lock_right == 0) ? grip_limit : 0;
+    lastButtonTime = now;
+  }
+
+  if (digitalRead(laserButR) == LOW) {      // Laser toggle
+    laserROn = !laserROn;
+    lastButtonTime = now;
+  }
+
+  // ----- LEFT HAND -----
+  if (digitalRead(gripButL) == LOW) {       // Grip toggle
+    des_grip_left = (des_grip_left == 0) ? grip_limit : 0;
+    lastButtonTime = now;
+  }
+
+  if (digitalRead(lockButL) == LOW) {       // Lock toggle
+    des_lock_left = (des_lock_left == 0) ? grip_limit : 0;
+    lastButtonTime = now;
+  }
+
+  if (digitalRead(laserButL) == LOW) {      // Laser toggle
+    laserLOn = !laserLOn;
+    lastButtonTime = now;
+  }
 }
 
 
 void loop() {
-  reading1 = digitalRead(button1);
-  reading2 = digitalRead(button2);
   currentTime = millis();
 
-  button_logic();
+  handleButtons();
 
-  keyboard_logic();
+  if (Serial.available()) {
+    String input = Serial.readStringUntil('\n');
+    input.trim();
 
-  control_logic();
+    char armID = input.charAt(0);
 
-  delay(50);
+    if (armID == 'R') {
+      int angle = input.substring(1).toInt();
+      angle = constrain(angle, 0, grip_limit);
+      des_grip_right = angle;
+    } 
+    else if (armID == 'L') {
+      int angle = input.substring(1).toInt();
+      angle = constrain(angle, 0, grip_limit);
+      des_grip_left = angle;
+    }
+    else if (armID == 'C') {
+      if (input.charAt(1) == 'L') {
+        int angle = input.substring(2).toInt();
+        des_lock_left = angle;
+      }
+      else if (input.charAt(1) == 'R') {
+        int angle = input.substring(2).toInt();
+        des_lock_right = angle;
+      }
+    }
+
+    digitalWrite(LED, LOW);
+  } 
+  else {
+    digitalWrite(LED, HIGH);  // blink if idle
+  }
+
+  // control the motors
+  if (curr_grip_left > des_grip_left) curr_grip_left -= 5;
+  else if (curr_grip_left < des_grip_left) curr_grip_left += 5;
+  gripServoL.write(curr_grip_left);
+  
+  if (curr_lock_left > des_lock_left) curr_lock_left -= 1;
+  else if (curr_lock_left < des_lock_left) curr_lock_left += 1;
+  lockServoL.write(curr_lock_left);
+
+  if (curr_grip_right > des_grip_right) curr_grip_right -= 5;
+  else if (curr_grip_right < des_grip_right) curr_grip_right += 5;
+  gripServoR.write(curr_grip_right);
+  
+  if (curr_lock_right > des_lock_right) curr_lock_right -= 1;
+  else if (curr_lock_right < des_lock_right) curr_lock_right += 1;
+  lockServoR.write(curr_lock_right);
+  
+
+  if (laserLOn) digitalWrite(laserL, HIGH);
+  else digitalWrite(laserL, LOW);
+
+  if (laserROn) digitalWrite(laserR, HIGH);
+  else digitalWrite(laserR, LOW);
+
+  updateLEDs();
+
+  int elapsed = millis() - currentTime;
+  if (elapsed < loopTime) delay(loopTime - elapsed);
 }
--- ARCLab_laproscopy/main.h
+++ ARCLab_laproscopy/main.h
@@ -1,140 +1,68 @@
 #include <PWMServo.h>
-
+#include <FastLED.h>
 
 // LEDs for debugging
 const int LED = 13;
 
+const int NUMLED = 5;
+const int rightLED = 21;
+const int leftLED = 20;
+CRGB leftLeds[NUMLED];
+CRGB rightLeds[NUMLED];
+
+int leftRGB[] = {0, 0, 0, 0};   // [R, G, B, W]
+int rightRGB[] = {0, 0, 0, 0};  // [R, G, B, W]
+
+int currentTime = 0;
+const int loopTime = 10;
+
+
+const int grip_limit = 50;
 
 // Motor Pinouts
-// Clamp motor
-const int clampCCW = 6;
-const int clampCW = 7;
 
+// Left Arm
+// laser motor
+const int laserL = 27;
+bool laserLOn = false;
 // Gripper Servo
-const int gripPWM = 2;
-PWMServo gripServo;
-int init_grip = 0;
-int curr_grip = 0;
-bool flip_grip = false;
-
+const int gripPWML = 4;
+PWMServo gripServoL;
+int des_grip_left = 0;
+int curr_grip_left = 0;
 // Stabilizer Lock Servo
-const int lockPWM = 3;
-PWMServo lockServo;
-int init_lock = 0;
-int curr_lock = 0;
-bool flip_lock = false;
+const int lockPWML = 5;
+PWMServo lockServoL;
+int des_lock_left = 0;
+int curr_lock_left = 0;
+
+// Right Arm
+// laser motor
+const int laserR = 26;
+bool laserROn = false;
+// Gripper Servo
+const int gripPWMR = 2;
+PWMServo gripServoR;
+int des_grip_right = 0;
+int curr_grip_right = 0;
+// Stabilizer Lock Servo
+const int lockPWMR = 3;
+PWMServo lockServoR;
+int des_lock_right = 0;
+int curr_lock_right = 0;
+
 
 // Buttons
-unsigned long debounceDelay = 1000; // ms
-unsigned long currentTime = 0;
+// Right hand
+const int laserButR = 16;
+const int gripButR = 41;
+const int lockButR = 17;
 
-// Clamp button
-const int button1 = 26;
-bool clampOn = true;
-bool switchDir = false;
+// Left hand
+const int laserButL = 32;
+const int gripButL = 31;
+const int lockButL = 10;
 
-// logic vars
-unsigned long lastDebounceTime1 = 0;
-int reading1 = HIGH;
-int lastButtonState1 = HIGH;
-
-// Grip button
-const int button2 = 27;
-bool servoOn = true;
-bool flip = false;
-
-// logic vars
-unsigned long lastDebounceTime2 = 0;
-int reading2 = HIGH;
-int lastButtonState2 = HIGH;
-
-
-// Functions
-void button_logic() {
-  // --- Clamp Button ---
-  if (reading1 == LOW && lastButtonState1 == HIGH) {
-    switchDir = !switchDir;
-    lastDebounceTime1 = currentTime;
-  }
-
-  if (reading1 == HIGH && lastButtonState1 == LOW) {
-    if (currentTime - lastDebounceTime1 > debounceDelay) {
-      clampOn = !clampOn;
-    }
-  }
-  lastButtonState1 = reading1;
-
-  // --- Servo Button ---
-  if (reading2 == LOW && lastButtonState2 == HIGH) {
-    flip = !flip;
-    lastDebounceTime2 = currentTime;
-  }
-
-  if (reading2 == HIGH && lastButtonState2 == LOW) {
-    if (currentTime - lastDebounceTime2 > debounceDelay) {
-      servoOn = !servoOn;
-    }
-  }
-  lastButtonState2 = reading2;
-}
-
-void keyboard_logic() {
-   if (Serial.available()) {
-    char key = Serial.read();
-
-    if (key == '1') { // switch servo
-      flip = !flip;
-    }
-    else if (key == '2') { // switch clamp dir
-      switchDir = !switchDir;
-    }
-    else if (key == '3') { // turn em off
-      servoOn = !servoOn;
-      clampOn = !clampOn;
-    }
-  }
-}
-
-void control_logic() {
-  // --- Control Logic ---
-  if (clampOn) {
-    if (switchDir) {
-      digitalWrite(clampCW, LOW);
-      digitalWrite(clampCCW, HIGH);
-    }
-    else {
-      digitalWrite(clampCW, HIGH);
-      digitalWrite(clampCCW, LOW);
-    }
-    digitalWrite(LED, HIGH);
-  } else {
-    digitalWrite(clampCW, LOW);
-    digitalWrite(clampCCW, LOW);
-    digitalWrite(LED, LOW);
-  }
-
-  if (servoOn) {
-    if (flip) {
-      // Use grip servo
-      if (flip_grip) curr_grip -= 5; 
-      else curr_grip += 5; 
-      
-      if (curr_grip >= 180 || curr_grip <= 0) { 
-        flip_grip = !flip_grip; 
-      }
-
-      gripServo.write(curr_grip);
-    }
-    else {
-      // Use grip servo
-      if (flip_lock) curr_lock -= 5; 
-      else curr_lock += 5; 
-      
-      if (curr_lock >= 180 || curr_lock <= 0) { 
-        flip_lock = !flip_lock; 
-      }
-
-      lockServo.write(curr_lock);
-    }
-  }
-}
+// Debounce timing
+unsigned long lastButtonTime = 0;
+const unsigned long debounceDelay = 150; // ms
